9b12194260cae3c72b1b32d8ff7511022d22372b
Carlos Rico <ricgarcar@gmail.com>
2023-08-11 13:28:02 +0200

    se añaden funciones para activar el event listener

--- js/main.js
+++ js/main.js
@@ -9,3 +9,65 @@ const images = [
 
 // APP state
 
+let index = 0;
+const max = images.length;
+
+// Get the DOM elements
+const containerElement = document.querySelector('.container')
+const prevSlideButton = document.querySelector('.prevSlide')
+const nextSlideButton = document.querySelector('.nextSlide')
+
+// Listen for arrow click events
+
+
+prevSlideButton.addEventListener('click', () =>{
+    index--
+    setImageIndex()
+    changeBackgroundImage(images[index],containerElement)
+
+
+    //Cambiar el fonde de imagen
+})
+
+    //Utility function
+
+    function setImageIndex(){
+        if (index<0){
+            index = max -1
+        }
+
+        if(index === max){
+            index = 0
+        }
+    }
+
+    function changeBackgroundImage(backgroundImage, element){
+        element.style.backgroundImage = `url(${backgroundImage})`
+    }
+
+
+
+    nextSlideButton.addEventListener('click', () =>{
+    index++
+    setImageIndex()
+    changeBackgroundImage(images[index],containerElement)
+
+
+    //Cambiar el fonde de imagen
+})
+
+    //Utility function
+
+    function setImageIndex(){
+        if (index<0){
+            index = max -1
+        }
+
+        if(index === max){
+            index = 0
+        }
+    }
+
+    function changeBackgroundImage(backgroundImage, element){
+        element.style.backgroundImage = `url(${backgroundImage})`
+    }
